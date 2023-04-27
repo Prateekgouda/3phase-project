38869c38eb9b3775f5c53bd45c1375795ead7771
Prateekgouda <124272526+Prateekgouda@users.noreply.github.com>
2023-04-27 17:35:29 +0530

    Add files via upload

---
+++ phase 3 project/SportyShoes/src/test/java/coms/SportyShoes/SportyShoesTest.java
@@ -0,0 +1,14 @@
+package coms.SportyShoes;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyShoesTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
